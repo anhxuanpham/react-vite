5384b1cc9ceb58ec3c3b8e4eaa4a828483f4e16c
William <anhxuanpham@hotmail.com>
2024-12-29 21:23:20 +0700

    30

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,6 +1,18 @@
+import './style.css';
+
+
+
+
+
 const MyComponent = () => {
     return (
-      <div>Hello William</div>
+    <>
+            <div>Hello William</div>
+            <div className="child"
+            style={{borderRadius: '10px'}}
+            >William</div>
+    </>
+      
     );
   }
 
