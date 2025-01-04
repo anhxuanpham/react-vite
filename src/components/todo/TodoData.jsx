5fb448eb338af7231aad17546e1b77c0db2425b1
William <anhxuanpham@hotmail.com>
2025-01-04 19:54:50 +0700

    webstorm

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -4,7 +4,7 @@ const TodoData = (props) => {
     
     const { name, age, data } = props;
 
-    // console.log(">>>> check props", props)
+    console.log(">>>> check props", props)
     return (
         <div className="todo-data">
         <div>William </div>
